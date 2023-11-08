41ebf49fce8ad773e4293a89eda3b8a218cb5e19
Shinovon <arman200711@gmail.com>
2023-11-08 19:09:07 +0500

    Create MIDlet class

---
+++ src/MahoRaspApp2.java
@@ -0,0 +1,20 @@
+import javax.microedition.midlet.MIDlet;
+
+public class MahoRaspApp2 extends MIDlet {
+
+	public MahoRaspApp2() {
+	}
+
+	protected void destroyApp(boolean unconditional) {
+
+	}
+
+	protected void pauseApp() {
+
+	}
+
+	protected void startApp() {
+
+	}
+
+}
